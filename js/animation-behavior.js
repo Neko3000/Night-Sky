a74592533b6135715d72cbdce39106ac4ff75735
Neko3000 <Neko3000@live.com>
2017-02-06 17:00:00 +0900

    vision 1.0

---
+++ js/animation-behavior.js
@@ -0,0 +1,57 @@
+$('.board').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible s-animated s-fadeInUp',
+    offset:100
+});
+
+$('.single-story-block-cell').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible s-animated s-fadeInUp',
+    offset:100
+});
+
+
+
+
+$('.single-constellation-block.cons-1').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible s-animated s-fadeInUp',
+    offset:100
+});
+
+$('.single-constellation-block.cons-2').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible s-animated s-fadeInUp',
+    offset:200
+});
+
+$('.single-constellation-block.cons-3').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible s-animated s-fadeInUp',
+    offset:300
+});
+
+
+
+
+$('.plain-text,.plain-img').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible s-animated s-fadeInUp',
+    offset:100
+});
+
+
+
+$('.poem').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible s-animated s-fadeInUp',
+    offset:100
+});
+
+$('.poem-block').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible animated fadeInUp',
+    offset:300
+});
+
+
+$('.waiting').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible s-animated s-fadeInUp',
+    offset:100
+});
+$('.waiting>span').addClass('a-hidden').viewportChecker({
+    classToAdd:'a-visible animated fadeIn',
+    offset:200
+});
